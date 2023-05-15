a688acd3ebbc2a049baa1b6a69efd1533edefaa1
lisamarimyrene <lisamy@stud.ntnu.no>
2023-05-15 13:51:37 +0200

    created test

--- src/assignment2/json_to_vcard_id_parser.py
+++ src/assignment2/json_to_vcard_id_parser.py
@@ -1,8 +1,8 @@
 # Denne parseren er inspirert fra IDG2001 Cloud Technologies Lab 3
-def serialize_data:
-    # Serialize the vCards to a list of strings in
-    vcard_str = vcard.serialize()
+def get_address_fields(address):
+    return address.split(';')[2:]
 
+def 
 
 # * This function finds a collection object based on id, and parses it from json to vcard
 def json_id_parser(id):
@@ -19,7 +19,6 @@ def json_id_parser(id):
     vcard = vobject.vCard()
     
     # Set the properties from the MongoDB data
-    # The get() adds a default text if the item doesn't exist.
     vcard_properties = {
         'birthday': 'birthday',
         'version': 'version',
@@ -29,23 +28,26 @@ def json_id_parser(id):
         'tel': 'telefon',
         'email': 'email'
     }
+
+    # The get() adds a default text if the item doesn't exist.
     for vcard_property, mongo_property in vcard_properties.items():
         value = data.get(mongo_property, f'No {vcard_property.capitalize()}')
         vcard.add(vcard_property).value = value
 
     address = data.get('address')
     if address:
-        address_fields = address.split(';')[2:]
+        address_fields = get_address_fields(address)
         street, city, region, code, country = address_fields + ['']*(5 - len(address_fields))
         vcard.add('adr').value = vobject.vcard.Address(
             street=street, city=city, region=region, code=code, country=country
         )
 
-    serialize_data()
+    # Serialize the vCards to a list of strings in
+    vcard_str = vcard.serialize()
 
     # Combine the vCard strings into a single JSON string
     # Source: https://docs.python.org/3/library/json.html
     vcards_id_json = json.dumps(vcard_str, indent=2)
-    
+
     # Return the output so we can access it in the api
     return {"message" : vcards_id_json}
