8770ca0e784d9521f6f52abcb09c16d9d539e7f0
lisamarimyrene <lisamy@stud.ntnu.no>
2023-05-15 19:21:00 +0200

    actions@

---
+++ tests/test_json_to_vcard_parser.py
@@ -0,0 +1,29 @@
+import json
+import vobject
+
+# Import functions that are going to be tested
+from assignment2.json_to_vcard_parser import get_address_fields, set_address_fields
+
+
+# Testing the function 'get_address_fields' and check if it splits on the semicolons. 
+def test_get_address_fields():
+    address = ";;One Microsoft Way;Redmond;WA;98052-6399;USA"
+
+    result = get_address_fields(address)
+
+    # Assert the correctness of the serialization
+    expected_result = ["One Microsoft Way", "Redmond", "WA", "98052-6399", "USA"]
+    assert result == expected_result
+
+
+def test_set_address_fields():
+    address_fields = ["One Microsoft Way", "Redmond"]
+
+    # Call the function
+    result = set_address_fields(address_fields)
+
+    # Define the expected result
+    expected_result = ["One Microsoft Way", "Redmond", "", "", ""]
+
+    # Assert that the Address object is created correctly
+    assert result == expected_result
