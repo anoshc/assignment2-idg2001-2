3b19ecf46e16b4cb07fb2d7ff383274bcb77be8b
lisamarimyrene <lisamy@stud.ntnu.no>
2023-05-15 13:02:37 +0200

    parser_id_test

--- src/assignment2/kode.py
+++ src/assignment2/kode.py
@@ -1,6 +1,26 @@
 import pytest
 
+from assignment2.json_to_vcard_id_parser import serialize_data
+
+# def data_test(x):
+#     return 3 * x
 
 
-def data_test(x):
-    return 3 * x
+def serialize_data():
+    # Mock the data and collection
+    mock_data = [
+        {
+            'name': 'John Doe',
+            'email': 'johndoe@example.com',
+            'telefon': '1234567890',
+        },
+        {
+            'name': 'Jane Smith',
+            'email': 'janesmith@example.com',
+            'telefon': '0987654321',
+        }
+    ]
+    # Mock the collection find method to return the mock data
+    collection.find = lambda: mock_data
+
+
